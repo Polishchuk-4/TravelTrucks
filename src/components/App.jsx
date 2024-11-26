130d1d58a00a99d0ed6dbb13d644e327d392306d
Denys Polishchuk <polischyk0000@gmail.com>
2024-11-26 14:48:47 +0200

    create route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "../pages/HomePage/HomePage";
+
 export default function App() {
-  return <>app</>;
+  return (
+    <Routes>
+      <Route index path="/" element={<HomePage />} />
+      <Route path="/catalog" element={<HomePage />} />
+      <Route path="/catalog/:id" element={<HomePage />} />
+    </Routes>
+  );
 }
